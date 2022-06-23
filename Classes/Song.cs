7f73dac1d35e592ebe856210b620c3446906a2e3
kais-hasson <108038908+kais-hasson@users.noreply.github.com>
2022-06-23 13:29:57 +0300

    Add files via upload

---
+++ Classes/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_project.Classes
+{
+    public class Song
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public int Size { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Classes/Students.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace School_project.Classes
+{
+    public class Students 
+    {
+        public int id { get; set; }
+        public string Full_name { get; set; }
+        public string Email { get; set; }
+        public string Father_name { get; set; }
+        public string Mother_name { get; set; }
+        public string Supervisor { get; set; }
+        public int Birth_date { get; set; }
+        public string Gender { get; set; }
+        public string Language { get; set; }
+        public string Language_ability { get; set; }
+        public string Language_study { get; set; }
+        public string Language_level { get; set; }
+        public int Phone_number { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string Message { get; set; }
+    }
+}
